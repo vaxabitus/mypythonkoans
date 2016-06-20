d9d312556cee17e30f78318ee62f78d98d4bc3dd
denislugovoj <denislugovoj@yandex.ru>
2016-06-20 19:02:49 +0300

    Last ver on 20 june 2016

--- koans/triangle.py
+++ koans/triangle.py
@@ -17,8 +17,14 @@
 #   about_triangle_project_2.py
 #
 def triangle(a, b, c):
-    # DELETE 'PASS' AND WRITE THIS CODE
-    pass
+    sides = [a, b, c]
+    uniqueSides = len(set(sides))
+    if uniqueSides == 1:
+        return 'equilateral'
+    elif uniqueSides == 2:
+        return 'isosceles'
+    else:
+        return 'scalene'
 
 # Error class used in part 2.  No need to change this code.
 class TriangleError(Exception):
